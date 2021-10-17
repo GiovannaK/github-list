bbbf8b0f256c59eeee438030f9b62ee5b57655a1
Giovanna Cunha <giovannakelli@gmail.com>
2021-10-17 02:04:46 -0300

    pwa functionality was added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,10 +9,12 @@ import createEmotionCache from '../createEmotionCache';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="pt-BR">
         <Head>
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
